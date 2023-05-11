0ee22914051f989533f36ab6359bac40c15b5e42
dfs1983 <d1983frederico@gmail.com>
2023-05-11 23:34:33 +0700

    first commit

---
+++ app/fungsi.py
@@ -0,0 +1,21 @@
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Activation, Dropout,LeakyReLU
+
+def make_model():
+    model = Sequential()
+    kernel = 3
+    model.add(Conv2D(filters=64, kernel_size=kernel, input_shape=(size_w, size_h, 3), activation='relu'))
+    model.add(MaxPool2D(pool_size=(2, 2)))
+    model.add(Conv2D(filters=128, kernel_size=kernel, activation='relu'))
+    model.add(MaxPool2D(pool_size=(2, 2)))
+    model.add(Conv2D(filters=256, kernel_size=kernel, activation='relu'))
+    model.add(MaxPool2D(pool_size=(2, 2)))
+    model.add(Conv2D(filters=512, kernel_size=kernel, activation='relu'))
+    model.add(MaxPool2D(pool_size=(2, 2)))
+    model.add(Flatten())
+    model.add(Dense(256, activation='relu'))
+    model.add(Dropout(0.2))
+    model.add(Dense(20, activation='softmax'))
+    
+    return model
