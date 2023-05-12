c964f92cd21d8764136bd0c3f87d74568c50557e
adindaauliaa <130948141+adindaauliaa@users.noreply.github.com>
2023-05-12 08:17:33 +0700

    Update fungsi.py

--- fungsi.py
+++ fungsi.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Activation, Dropout,LeakyReLU
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, MaxPool2D, Flatten, Dense, Activation, Dropout,LeakyReLU
 
 def make_model():
     model = Sequential()
